18b895f8d8593448f83cd68846928d4ab7eee8ba
Yonghui Tao <ytao624@aucklanduni.ac.nz>
2019-01-15 14:08:31 +1300

    qlearning

--- Q-Learning/QL-cartPole-matt.py
+++ Q-Learning/QL-cartPole-matt.py
@@ -4,43 +4,61 @@ import random
 import math
 from time import sleep
 
-# Initialize the "Cart-Pole" environment
+## Result: https://gym.openai.com/evaluations/eval_QykiR71DR0mpXEXslkpDLQ
+
+## Initialize the "Cart-Pole" environment
 env = gym.make('CartPole-v0')
 
-# Defining the environment related constants
-NUM_STATE = 10
-NUM_ACTION = env.action_space.n
-ANGLE_STATE_INDEX = 2
-ANGLE_BOUND = (-env.theta_threshold_radians*1.2, env.theta_threshold_radians*1.2)
-MIN_EXPLORE_RATE = 0.1
-MIN_LEARNING_RATE = 0.1
+## Defining the environment related constants
+
+# Number of discrete states (bucket) per state dimension
+NUM_BUCKETS = (1, 1, 6, 3)  # (x, x', theta, theta')
+# Number of discrete actions
+NUM_ACTIONS = env.action_space.n # (left, right)
+# Bounds for each discrete state
+STATE_BOUNDS = list(zip(env.observation_space.low, env.observation_space.high))
+# Manually setting bounds (needed for the x_dot and theta_dot)
+#STATE_BOUNDS[0] = [STATE_BOUNDS[0][0]/2, STATE_BOUNDS[0][1]/2]
+STATE_BOUNDS[1] = [-0.5, 0.5]
+#STATE_BOUNDS[2] = [STATE_BOUNDS[2][0]/2, STATE_BOUNDS[2][0]/2]
+STATE_BOUNDS[3] = [-math.radians(50), math.radians(50)]
+
+## Creating a Q-Table for each state-action pair
+q_table = np.zeros(NUM_BUCKETS + (NUM_ACTIONS,))
+
+## Learning related constants
+MIN_EXPLORE_RATE = 0.01
+MIN_LEARNING_RATE = 0.2
 
 # Defining the simulation related constants
-NUM_EPISODES = 400
-MAX_T = 500
-DEBUG_MODE = True
+NUM_EPISODES = 1000
+MAX_T = 250
+STREAK_TO_END = 120
+SOLVED_T = 199
+DEBUG_MODE = False
 ENABLE_UPLOAD = False
 
-# Creating a Q-Table for each action and state pair
-q_table = np.zeros((NUM_STATE, NUM_ACTION))
 
 if ENABLE_UPLOAD:
-    env.monitor.start('/tmp/cartpole-experiment-1', force=True)
+    env.monitor.start('/tmp/cart_pole_q_learning_4D', force=True)
 
 
 def simulate():
-    # Defining the learning related parameters
-    learning_rate = 0.2
-    explore_rate = 0.8
-    discount_factor = 0.999
+
+    ## Instantiating the learning related parameters
+    learning_rate = get_learning_rate(0)
+    explore_rate = get_explore_rate(0)
+    discount_factor = 0.999  # since the world is unchanging
+
+    num_streaks = 0
 
     for episode in range(NUM_EPISODES):
+
         # Reset the environment
         obv = env.reset()
 
         # the initial state
-        state_0 = angle_to_bucket(obv[ANGLE_STATE_INDEX])
-        t_done = 0
+        state_0 = state_to_bucket(obv)
 
         for t in range(MAX_T):
             env.render()
@@ -49,45 +67,46 @@ def simulate():
             action = select_action(state_0, explore_rate)
 
             # Execute the action
-            obv, reward, done, info = env.step(action)
+            obv, reward, done, _ = env.step(action)
 
             # Observe the result
-            angle = obv[ANGLE_STATE_INDEX]
-            state = angle_to_bucket(angle)
-
+            state = state_to_bucket(obv)
 
             # Update the Q based on the result
-            best_q = np.amax(q_table[state,:])
-            #q_table[state_0, action] += learning_rate*(reward + discount_factor*(best_q) - q_table[state_0, action])
+            best_q = np.amax(q_table[state])
+            q_table[state_0 + (action,)] += learning_rate*(reward + discount_factor*(best_q) - q_table[state_0 + (action,)])
 
             # Setting up for the next iteration
             state_0 = state
-            if not done:
-                t_done = t
 
             # Print data
             if (DEBUG_MODE):
                 print("\nEpisode = %d" % episode)
                 print("t = %d" % t)
                 print("Action: %d" % action)
-                print("Angle: %f" % angle)
-                print("State: %d" % state)
+                print("State: %s" % str(state))
                 print("Reward: %f" % reward)
                 print("Best Q: %f" % best_q)
                 print("Explore rate: %f" % explore_rate)
                 print("Learning rate: %f" % learning_rate)
-                print("Q Table")
-                q_table_t = np.transpose(q_table)
-                for col in q_table_t:
-                    for q in col:
-                        # print("%.2f" % q, end="  ")
-                    # print("")
+                print("Streaks: %d" % num_streaks)
 
-            if angle < -env.theta_threshold_radians*2 or angle > env.theta_threshold_radians*2 :
-               print("Episode %d finished after %f time steps" % (episode, t_done))
-               break
+                print("")
 
-            #sleep(0.25)
+            if done:
+                print("Episode %d finished after %f time steps" % (episode, t))
+
+                if t >= SOLVED_T:
+                    num_streaks += 1
+                else:
+                    num_streaks = 0
+                break
+
+        #sleep(0.25)
+
+        # It's considered done when it's solved over 120 times consecutively
+        if num_streaks > STREAK_TO_END:
+            break
 
         # Update parameters
         explore_rate = get_explore_rate(episode)
@@ -95,7 +114,7 @@ def simulate():
 
     if ENABLE_UPLOAD:
         env.monitor.close()
-        gym.upload('/tmp/cartpole-experiment-1',
+        gym.upload('/tmp/cart_pole_q_learning_4D',
                    api_key='sk_93AMQvdmReWCi8pdL4m6Q')
 
 
@@ -105,26 +124,31 @@ def select_action(state, explore_rate):
         action = env.action_space.sample()
     # Select the action with the highest q
     else:
-        action = np.argmax(q_table[state,:])
+        action = np.argmax(q_table[state])
     return action
 
 
 def get_explore_rate(t):
-    return min(1, MIN_EXPLORE_RATE*NUM_EPISODES/(t+1))
+    return max(MIN_EXPLORE_RATE, min(1, 1.0 - math.log10((t+1)/25)))
 
 def get_learning_rate(t):
-    return min(0.8, MIN_LEARNING_RATE*NUM_EPISODES/(t+1))
+    return max(MIN_LEARNING_RATE, min(0.5, 1.0 - math.log10((t+1)/25)))
 
-
-def angle_to_bucket(angle):
-    angle = min(ANGLE_BOUND[1], max(ANGLE_BOUND[0], angle))
-    return int((angle - ANGLE_BOUND[0])/(ANGLE_BOUND[1] - ANGLE_BOUND[0])*(NUM_STATE-1))
-
-
-def bucket_to_angle(state):
-
-    return state / (NUM_STATE-1) * (ANGLE_BOUND[1] - ANGLE_BOUND[0]) + ANGLE_BOUND[0]
-
+def state_to_bucket(state):
+    bucket_indice = []
+    for i in range(len(state)):
+        if state[i] <= STATE_BOUNDS[i][0]:
+            bucket_index = 0
+        elif state[i] >= STATE_BOUNDS[i][1]:
+            bucket_index = NUM_BUCKETS[i] - 1
+        else:
+            # Mapping the state bounds to the bucket array
+            bound_width = STATE_BOUNDS[i][1] - STATE_BOUNDS[i][0]
+            offset = (NUM_BUCKETS[i]-1)*STATE_BOUNDS[i][0]/bound_width
+            scaling = (NUM_BUCKETS[i]-1)/bound_width
+            bucket_index = int(round(scaling*state[i] - offset))
+        bucket_indice.append(bucket_index)
+    return tuple(bucket_indice)
 
 if __name__ == "__main__":
     simulate()
